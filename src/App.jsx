3f262b9e1e9a61c79c2ea838fb33a5f85aac96f8
Mercede Chen <mercedechen@gmail.com>
2024-04-11 20:09:23 -0400

    Add navigation and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
+// React
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import Home from "./components/Home.jsx";
+import Navigation from "./components/Navigation.jsx";
+import About from "./components/About.jsx";
+import Projects from "./components/Projects.jsx";
+import Extras from "./components/Extras.jsx";
+
 export default function App() {
   return (
     <>
+      <BrowserRouter>
+        <Navigation/>
+
+        <Routes>
+          <Route 
+            path="/"
+            element={<Home/>}
+          />
+          <Route
+            path="/about"
+            element={<About/>}
+          />
+          <Route 
+            path="/projects"
+            element={<Projects/>}
+          />
+          <Route
+            path="/extras"
+            element={<Extras/>}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,18 @@
+// React
+import { NavLink } from "react-router-dom";
+
+// Components
+import Socials from "../components/Socials";
+
 export default function Navigation(){
   return (
     <>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/projects">Projects</NavLink>
+      <NavLink to="/extras">Extras</NavLink>
+      
+      <Socials/>
     </>
   )
 }
