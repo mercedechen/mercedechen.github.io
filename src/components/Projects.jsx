1b5e82c693361532b9ad875b5111c474b267e110
Mercede Chen <mercedechen@gmail.com>
2024-04-13 15:30:44 -0400

    Add styles to projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,16 +4,18 @@ import "../styles/projects.css";
 export default function Projects(){
   return (
     <div className="container">
-      <h3>Creative Design Services for the College of Arts and Sciences, University at Buffalo</h3>
-      {
-        casDataCard?.map((item) => {
-          return (
-            <div key={item.id}>
-              <img src={item.img}/>
-            </div>
-          )
-        })
-      }
+      <div className="internship">
+        <h3>Creative Design Services for the College of Arts and Sciences, University at Buffalo</h3>
+        {
+          casDataCard?.map((item) => {
+            return (
+              <div key={item.id}>
+                <img src={item.img}/>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
