b24c5a9fe702d3c15e2ed600ca254795cac349d4
Mercede Chen <mercedechen@gmail.com>
2024-04-12 23:05:26 -0400

    Add data to image carousel and minor css fix

--- src/data/carouselData.js
+++ src/data/carouselData.js
@@ -1,6 +1,7 @@
 import img1 from "../assets/images/4556.jpg";
 import img2 from "../assets/images/279.jpg";
 import img3 from "../assets/images/0033.jpg";
+import img4 from "../assets/images/the-getty-center.jpg";
 
 export const carouselData = [
   {
@@ -17,5 +18,10 @@ export const carouselData = [
     id: 3,
     img: img3,
     alt: "Winter at Wolfe's Pond, 2019"
+  },
+  {
+    id: 4,
+    img: img4,
+    alt: "The Getty Center, 2022"
   }
 ]
